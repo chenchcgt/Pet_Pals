6f0dbfc7fd0363ffd2008f20318fc6275285d5e6
Hung Chen <chen_chc@hotmail.com>
2020-10-20 18:32:28 -0400

    commit initdb

--- initdb.py
+++ initdb.py
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
-# from app import db
-=======
+# <<<<<<< HEAD
+# # from app import db
+# =======
 from Pet_Pals.app import db
->>>>>>> 28b117c73b55ffcc2a42fba7a4513696b74ca279
+# >>>>>>> 28b117c73b55ffcc2a42fba7a4513696b74ca279
 
 # db.drop_all()
 db.create_all()
